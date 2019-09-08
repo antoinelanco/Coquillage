184fc94d2075b1655fa8433b6f8f8ff9b7a30003
kerkristof <kerkristof@gmail.com>
2019-09-08 14:54:42 +0200

    Christophe

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-print ("Hello world")
+bc = []
+# mvt 1
+move = [1,2,100]
+bc.append (move)
+# mvt 2
+move = [1,2,100]
+bc.append (move)
+# mvt 3
+move = [3,1,500]
+bc.append(move)
+
+
+# amount user1
+amount = 0
+for value in bc:
+    if value[0] == 1:
+        amount -= value[2]
+    elif value[1] == 1:
+        amount += value[2]
+print amount
+# amount user2
+amount = 0
+for value in bc:
+    if value[0] == 2:
+        amount -= value[2]
+    elif value[1] == 2:
+        amount += value[2]
+print amount
+# amount user3
+amount = 0
+for value in bc:
+    if value[0] == 3:
+        amount -= value[2]
+    elif value[1] == 3:
+        amount += value[2]
+print amount
