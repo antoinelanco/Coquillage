717a62da13aa49253dd01c76968a34683bd4607e
antoinelanco <antoinelanco@gmail.com>
2019-09-10 15:08:07 +0200

    python3

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 bc = []
 users = {"user0":1000}
 
@@ -13,18 +15,18 @@ bc.append (move)
 move = ["user3","user4",250]
 bc.append (move)
 
-print bc
+print(bc)
 
 for value in bc:
     list = (0,1)
     for i in list:
         if not users.get(value[i]):
-            print "On genere le " + value[i]
+            print ("On genere le " + value[i])
             users[value[i]] = 0
     # effectue la transaction
     users[value[0]] -= value[2]
     users[value[1]] += value[2]
-    print users
+    print(users)
 
 for key, value in users.items():
-    print key + " get " + str(value) + " coquillages"
+    print (key + " get " + str(value) + " coquillages")
