f45ba6ba17a9e01e72be918d9570d1ff2d0fec34
antoinelanco <antoinelanco@gmail.com>
2019-09-12 12:24:19 +0200

    server

---
+++ web.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def api_root():
+    return "Welcome"
+
+if __name__ == '__main__':
+	app.run("192.168.201.64")
