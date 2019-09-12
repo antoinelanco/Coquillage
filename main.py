c3154018c9d7736de22cff6fff7e43302c1c484f
kerkristof <kerkristof@gmail.com>
2019-09-12 11:59:09 +0200

    t

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 #! /usr/bin/env python3
 import block
 
-
-
 def add_dict(dict,key,value):
     if not key in dict:
         dict[key] = 0
@@ -18,36 +16,7 @@ def amount(bc):
     for v in bc:
         add_dict(users,v[1],v[2])
         sub_dict(users,v[0],v[2])
-
-
-
-
     print(users)
-        #     print ("On genere le " + value[i])
-        #     users[value[i]] = 0
-        #
-        # users[v[1]] +=
-        # print(value)
-
-
-# for value in bc:
-#     list = (0,1)
-#     for i in list:
-#         if not users.get(value[i]):
-#             print ("On genere le " + value[i])
-#             users[value[i]] = 0
-#     # effectue la transaction
-#     users[value[0]] -= value[2]
-#     users[value[1]] += value[2]
-#     print(users)
-
-
-
-#
-# for key, value in users.items():
-#     print (key + " get " + str(value) + " coquillages")
-
-
 
 bc = []
 
@@ -66,5 +35,5 @@ bc.append (move)
 move = ["user4","user5",250]
 bc.append (move)
 
-# print(bc)
+print(bc)
 amount(bc)
